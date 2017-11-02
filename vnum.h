be476860f1c4b64d89aa3cb22731dd2dc49ad4b7
Jacob Smith <jaroslov@gmail.com>
2017-11-01 22:07:45 -0500

    [miscc] Update the copyright to reflect the fact I wrote this in 2017.

--- vnum.h
+++ vnum.h
@@ -1,6 +1,6 @@
 /*
 
-Copyright (c) 2014, Jacob N. Smith
+Copyright (c) 2017, Jacob N. Smith
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
